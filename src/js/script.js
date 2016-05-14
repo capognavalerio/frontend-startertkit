01a3f8525cbce68a1a7105a94390c4ac541af115
valerio <valerio@basili.co>
2016-05-14 18:42:28 +0200

    add google web font

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
+// Load Lato Font Family from google web font
+WebFontConfig = {
+  google: { families: [ 'Lato:400,300,100,100italic,300italic,400italic,700,900,700italic,900italic:latin' ] }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
+
+
 (function($, FastClick, outdatedBrowser) {
 
   // Initialize Fastclick to remove click delays on browsers with touch UIs
