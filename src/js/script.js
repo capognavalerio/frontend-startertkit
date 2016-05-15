6a18ca6035beb3282cf759e50c1106e77b318738
valerio <valerio@basili.co>
2016-05-15 10:45:22 +0200

    remove google web fonts

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,3 @@
-// Load Lato Font Family from google web font
-WebFontConfig = {
-  google: { families: [ 'Lato:400,300,100,100italic,300italic,400italic,700,900,700italic,900italic:latin' ] }
-};
-(function() {
-  var wf = document.createElement('script');
-  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
-})();
-
 
 (function($, FastClick, outdatedBrowser) {
 
